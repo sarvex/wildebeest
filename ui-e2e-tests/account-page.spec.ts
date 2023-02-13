c3619c92ebe58fc69a107846fafdc44daa0729d4
Dario Piotrowicz <dario@cloudflare.com>
2023-02-13 22:38:38 +0000

    test infinite scrolling on account and explore page

--- ui-e2e-tests/account-page.spec.ts
+++ ui-e2e-tests/account-page.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test'
+import { getMockStatusFn } from 'wildebeest/ui-e2e-tests-utils/getMockStatusFn'
 
 test('Navigation to and view of an account (with 1 post)', async ({ page }) => {
 	await page.goto('http://127.0.0.1:8788/explore')
@@ -38,4 +39,33 @@ test('Navigation to and view of an account (with 2 posts)', async ({ page }) => 
 	await expect(post1Locator).toContainText("I'm Rafa, a designer and app developer currently living in Amsterdam")
 	await expect(post2Locator.getByRole('img', { name: 'Avatar of Rafa' })).toBeVisible()
 	await expect(post2Locator).toContainText('Hi, meet HiDock!')
+})
+
+test('Fetching of new account toots on scroll (infinite scrolling)', async ({ page }) => {
+	const generateFakeStatus = getMockStatusFn()
+	await page.route('http://127.0.0.1:8788/api/v1/accounts/rafa/statuses?*', async (route) => {
+		const newStatuses = new Array(5).fill(null).map(generateFakeStatus)
+		await route.fulfill({ body: JSON.stringify(newStatuses) })
+	})
+
+	await page.goto('http://127.0.0.1:8788/explore')
+	await page.locator('article').filter({ hasText: "I'm Rafa, a designer and app" }).locator('i.fa-globe + span').click()
+	await page.waitForLoadState('networkidle')
+	await page.getByRole('link', { name: 'Rafa', exact: true }).click()
+
+	for (let i = 0; i < 3; i++) {
+		await page.mouse.wheel(0, Number.MAX_SAFE_INTEGER)
+		for (let j = 0; j < 5; j++) {
+			const paddedJ = `${i * 5 + j}`.padStart(3, '0')
+			// check that the new toots have been loaded
+			await expect(page.locator('article').filter({ hasText: `Mock Fetched Status #${paddedJ}` })).toBeVisible()
+		}
+		const paddedExtra = `${i * 5 + 6}`.padStart(3, '0')
+		// check that a 6th toot hasn't been loaded (since the mock endpoint returns 5 toots)
+		await expect(page.locator('article').filter({ hasText: `Mock Fetched Status #${paddedExtra}` })).not.toBeVisible()
+	}
+
+	const numOfMockFetchedToots = await page.locator('article').filter({ hasText: `Mock Fetched Status` }).count()
+	// check that all 15 toots have been loaded
+	expect(numOfMockFetchedToots).toBe(15)
 })
